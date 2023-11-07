8de788c451cd64071baed02f73ba872dfb9fbb58
Scholar <scholar@paradigm.local>
2023-11-07 14:55:49 -0700

    I did it wrong

--- VeryDataTypable/VaDT.js
+++ VeryDataTypable/VaDT.js
@@ -1,15 +1,84 @@
-//3 Strings "", 3 Numbers 0, 3 Boolean true/false, 3 Array let blank = blank, 3 Object
-const myName = "Elena"
+    //3 Strings "", 3 Numbers 0, 3 Boolean true/false, 3 Array let blank = blank, 3 Object
+    // Done: 3 Numbers. 3 Strings, 3 Booleons, 
 
-let favColor = "Green or Blue"
 
-favColor = "Green"
+    // 3 Strings
 
-let age = 17
+    const myName = "Elena"
 
-const isAlive = true
+    eyeColor = "Hazel"
 
-const isGirl = true
+    favFoods = "Apple"
 
-const isGraduating = true
+    // 3 Boolean
 
+    const isAlive = true
+
+    const isGirl = true
+
+    const isGraduating = true
+
+    // 3 Number
+
+    let birthmonth = 1
+
+    let birthday = 11
+
+    let age = 17
+
+    // 3 Array
+
+    let favSeason = ["Spring", "Fall", "Summer"]
+
+    let favColor = ["Green", "Blue", "Purple"]
+
+    let favSibling = ["Soren", "Talia", "Mara"]
+
+    // 3 Object
+
+    Isitaperson = {
+        name: "Frankenstein's Monster",
+        age: 0,
+        favColor: "What's a Color",
+        favFoods: ["I", "Don't", "Know"]
+    }
+
+    ItISaperson = {
+        name: "Dr. Satan",
+        age: 152,
+        favColor: "Blood-Red",
+        favFoods: ["Apple Sauce", "Popcorn", "Jamba Juice"],
+    }
+
+    ItsaDogperson = {
+        name: "Larry Talbot",
+        age: 36,
+        favMovie: ["It's the Great Pumpkin, Charlie Brown", "The Hero of Color City"],
+        favGame: "ToonTown Online",
+    }
+
+    document.getElementById("title").innerHTML = `Hello, My name is ${myName}`
+
+    document.getElementById("title1").innerHTML = `My eye color is ${eyeColor}`
+
+    document.getElementById("title2").innerHTML = `My favorite food is ${favFoods}`
+
+    document.getElementById("title3").innerHTML = `I am alive = ${isAlive}`
+
+    document.getElementById("title4").innerHTML = `I am a girl = ${isGirl}`
+
+    document.getElementById("title5").innerHTML = `I am graduating = ${isGraduating}`
+    
+    document.getElementById("title6").innerHTML = `My name birth month is ${myName}`
+
+    document.getElementById("title7").innerHTML = `My name is birth date is ${myName}`
+
+    document.getElementById("title8").innerHTML = `My age is ${age}`
+
+    document.getElementById("title9").innerHTML = `My favorite seasons are ${favSeason}`
+
+    document.getElementById("title10").innerHTML = `My favorite colors are ${favColor}`
+
+    document.getElementById("title11").innerHTML = `My favorite siblings are ${favSibling}`
+
+    document.getElementById("title12").innerHTML = `${Isitaperson}`
