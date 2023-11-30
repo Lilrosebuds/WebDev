94d20c9f4d1c51ed5f087bbb31a085fca3f79e3a
Scholar <scholar@paradigm.local>
2023-11-30 14:56:33 -0700

    start

---
+++ Finaldn/finale.js
@@ -0,0 +1,22 @@
+//3 unique pages each with:
+//A header
+//A nav bar
+//Your content
+//A footer with:
+//A site map
+//Contact info
+//Minimum skills you need to showcase on your project include:
+//Utilizing tags, elements, and semantic elements properly
+//A form with:
+//2 input elements
+//1 checkbox element
+//1 set of radio buttons
+//1 select element
+//1 button
+//Meta tags on each of your HTML files
+//Use of box model to add padding, margin, and a border
+//Use of either display: grid or display: flex with its corresponding properties
+//Media queries for 3 screen sizes
+//1 animation per page
+//Use of at least 2 functions
+//Use of at least 2 event listeners
